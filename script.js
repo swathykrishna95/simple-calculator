0e738342f68f108ba003e356a39161c2a40a1969
Swathy Krishna <138104838+swathykrishna95@users.noreply.github.com>
2023-07-24 11:34:04 +0530

    Add addition functionality

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let add =(num1, num2) => {
-    console.log(num1 + num2);
+    display.innerText+=(num1 + num2);
 }
 
 let subtract =(num1, num2) => {
@@ -34,4 +34,31 @@ let operate = (num1, operator, num2) => {
     }
 }
 
-operate(23, '+', 45);
+//operate(23, '+', 45);
+let displayValue = "";
+let display = document.querySelector('#display');
+let buttons = document.querySelectorAll('button');
+let clear = document.querySelector('#clear');
+buttons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        if(e.target.textContent == 'C'){
+            //console.log(e.target.textContent);
+            display.innerText = '';
+            displayValue = '';
+        }
+        else{
+            displayValue += e.target.textContent;
+            display.innerText = displayValue;
+            if(e.target.textContent === '='){
+            let temp = displayValue.slice(0,-1);
+            if(displayValue.includes('+')){
+                let nums = temp.split('+');
+                console.log(nums);
+                operate(parseInt(nums[0]),'+', parseInt(nums[1]));
+            }
+        }
+        
+        }
+    });
+});
+
